1d7686a4331181b262feea7c534d46a6ed5b37d8
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-14 19:19:59 -0300

    Coment and live code for reference

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,19 @@
 class Contact < ApplicationRecord
   belongs_to :kind
-  
-  def author
-    'Hebe Camargo'
-  end
 
-  def as_json(options={})
-    super(methods: :author, root: true)
-  end
+  # def author
+  #   'Hebe Camargo'
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   super(
+  #     methods: [:author, :kind_description],
+  #     root: true,
+  #     include: :kind
+  #    )
+  # end
 end
