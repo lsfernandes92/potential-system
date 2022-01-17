831506b387297fcf5b9916df68cc5991a0dd9e50
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-17 13:20:10 -0300

    Create new model Phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_213226) do
+ActiveRecord::Schema.define(version: 2022_01_17_145531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,14 @@ ActiveRecord::Schema.define(version: 2022_01_14_213226) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "phones", force: :cascade do |t|
+    t.string "number"
+    t.bigint "contact_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_phones_on_contact_id"
+  end
+
   add_foreign_key "contacts", "kinds"
+  add_foreign_key "phones", "contacts"
 end
