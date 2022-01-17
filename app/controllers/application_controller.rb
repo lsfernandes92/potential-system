6162d7eee3f54cbf74f8c7b545ce12b2a3d80ed2
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-17 17:56:40 -0300

    Enforce client send the right media time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+  before_action :ensure_json_mime_type
+
+  def ensure_json_mime_type
+    return if request.headers['Accept'] == 'application/vnd.api+json'
+    render nothing: true, status: :not_acceptable
+  end
 end
