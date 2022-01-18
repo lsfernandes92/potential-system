93f1df76d175fcfe1ed0f2fc79786a947f1b5686
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-18 19:37:39 -0300

    Add link to Contact association related

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
   belongs_to :kind do
-    link(:related) { kind_url(object.kind.id) }
+    link(:related) { contact_kind_url(object.id) }
   end
-  has_many :phones
-  has_one :address
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id) }
+  end
+  has_one :address do
+    link(:related) { contact_address_url(object.id) }
+  end
 
   attributes :id, :name, :email, :birthdate
 
