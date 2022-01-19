1d5b2d7c499573290a162ca463f68249558a7a5b
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-19 11:57:30 -0300

    Add link api pagination

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,9 @@ class ContactsController < ApplicationController
 
   # GET /contacts
   def index
-    @contacts = Contact.all
+    page_number = params[:page].try(:[], :number)
+    per_page = params[:page].try(:[], :size)
+    @contacts = Contact.all.page(page_number).per(per_page)
 
     render json: @contacts
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,8 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address
 
+  paginates_per 5
+
   def as_json(options={})
     h = super(options.merge(include: [:kind, :phones, :address]))
     h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.nil?)
