61cdfb9bd42e416d2a89df246c01d01cec1d33b4
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-19 11:22:35 -0300

    Polish Mime Types needed in requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,31 @@
 class ApplicationController < ActionController::API
 
-  before_action :ensure_json_mime_type
+  before_action :ensure_mime_types
 
-  def ensure_json_mime_type
-    return if request.headers['Accept'] == 'application/vnd.api+json'
-    render nothing: true, status: :not_acceptable
+  def ensure_mime_types
+    ensure_accept
+    ensure_content_type
   end
+
+  private
+
+    def ensure_accept
+      return if accept?
+      render nothing: true, status: :not_acceptable
+    end
+
+    def ensure_content_type
+      unless request.get?
+        return if content_type?
+        render nothing: true, status: :unsupported_media_type
+      end
+    end
+
+    def content_type?
+      request.headers["Content-Type"] == 'application/vnd.api+json'
+    end
+
+    def accept?
+      request.headers['Accept'] == 'application/vnd.api+json'
+    end
 end
